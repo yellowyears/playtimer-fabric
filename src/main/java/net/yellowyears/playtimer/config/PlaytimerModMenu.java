13f14439a98590168749be2b23d8ecafb199aca6
yellowyears <yellowyears200@gmail.com>
2022-07-14 12:44:27 +0100

    Remove remappedSrc folder

--- src/main/java/net/yellowyears/playtimer/config/PlaytimerModMenu.java
+++ src/main/java/net/yellowyears/playtimer/config/PlaytimerModMenu.java
@@ -5,7 +5,6 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screen.Screen;
 
 @Environment(EnvType.CLIENT)
 public class PlaytimerModMenu implements ModMenuApi{
