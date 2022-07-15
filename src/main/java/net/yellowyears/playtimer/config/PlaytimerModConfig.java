d220cc7b3d215ed6ff44b806b0890f65dc9f5ecf
yellowyears <yellowyears200@gmail.com>
2022-07-15 21:10:55 +0100

    Add cycle button for timer position

--- src/main/java/net/yellowyears/playtimer/config/PlaytimerModConfig.java
+++ src/main/java/net/yellowyears/playtimer/config/PlaytimerModConfig.java
@@ -3,10 +3,23 @@ package net.yellowyears.playtimer.config;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.EnumHandler;
 
 @Config(name = "playtimer")
 public class PlaytimerModConfig implements ConfigData {
     @ConfigEntry.ColorPicker
     public int colour = 0xFF5555;
+
+    @EnumHandler(option = EnumHandler.EnumDisplayOption.BUTTON)
+    public PlaytimerPosition playtimerPosition = PlaytimerPosition.BOTTOM_RIGHT;
+
+    public static enum PlaytimerPosition {
+        TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT;
+
+        @Override
+        public String toString() {
+            return "playtimer.theme." + this.name().toLowerCase();
+        }
+    }
 
 }
--- src/main/java/net/yellowyears/playtimer/config/PlaytimerModMenu.java
+++ src/main/java/net/yellowyears/playtimer/config/PlaytimerModMenu.java
@@ -6,10 +6,13 @@ import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
+
 @Environment(EnvType.CLIENT)
 public class PlaytimerModMenu implements ModMenuApi{
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> AutoConfig.getConfigScreen(PlaytimerModConfig.class, parent).get();
     }
+
 }
